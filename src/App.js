4da22309ab45aa9dd881513df9175e15ac208c03
Klaway <78068405+Klaway@users.noreply.github.com>
2025-07-13 13:52:24 +0300

    Import ./utils/cookies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-
-const setCookie = (name, value, days) => {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-};
-
-const getCookie = (name) => {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split('; ');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-};
+import { setCookie, getCookie } from './utils/cookies';
 
 // --- Theme Toggle Switch Component ---
 const ThemeToggle = ({ isDarkMode, toggleDarkMode }) => {
